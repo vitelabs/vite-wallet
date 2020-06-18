47005420162ba11636e4c63fd6e7434acb9add0f
soliury <soliury@pm.me>
2020-06-18 19:19:52 +0800

    get notification permission when start app

--- walletSrc/modules/init/preload.js
+++ walletSrc/modules/init/preload.js
@@ -9,3 +9,19 @@ window.viteWalletRequest = appHttp;
 window.viteWalletI18n = appI18n;
 window.DESKTOP = true;
 window.ipcRenderer = ipcRenderer;
+
+if (Notification.permission === "granted") {
+    console.info('Notification is granted.');
+} else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+        if (permission === "granted") {
+            console.log('Got notification permission');
+        }
+        if (permission === 'default') {
+            console.log('The permission request was dismissed.');
+        }
+        if (permission === 'denied') {
+            console.log('Permission wasn\'t granted. Allow a retry.');
+        }
+    });
+}
