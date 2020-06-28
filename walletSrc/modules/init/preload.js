1bef2b26e517fad57dfa0d1a1a53c0717e5781e5
soliury <soliury@pm.me>
2020-06-28 21:16:49 +0800

    remove notification permission request from desktop code

--- walletSrc/modules/init/preload.js
+++ walletSrc/modules/init/preload.js
@@ -13,19 +13,3 @@ window.ipcRenderer = ipcRenderer;
 if (process.platform === 'darwin') {
     window.touchID = touchID;
 }
-
-if (Notification.permission === "granted") {
-    console.info('Notification is granted.');
-} else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-        if (permission === "granted") {
-            console.log('Got notification permission');
-        }
-        if (permission === 'default') {
-            console.log('The permission request was dismissed.');
-        }
-        if (permission === 'denied') {
-            console.log('Permission wasn\'t granted. Allow a retry.');
-        }
-    });
-}
